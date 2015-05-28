94c91467c114cb25311e1432d44993636d2aaa7b
neeting <linoleumaream@gmail.com>
2015-05-29 04:12:51 +0900

    first

---
+++ Gemfile
@@ -0,0 +1,7 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+# gem "rails"
+gem 'jekyll'
+gem 'redcarpet'
+gem 'wdm', '>= 0.1.0' if Gem.win_platform?
---
+++ vendor/bundler/ruby/2.0.0/bin/safe_yaml.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+IF NOT "%~f0" == "~f0" GOTO :WinNT
+@"ruby.exe" "D:/System/Users/Master/Documents/GitHub/hyde/vendor/bundler/ruby/2.0.0/bin/safe_yaml" %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO :EOF
+:WinNT
+@"ruby.exe" "%~dpn0" %*
